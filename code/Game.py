816406ec4f09839860f2780cc66417abe92dab64
larissa <larissafrazak4@gmail.com>
2026-03-23 08:57:11 -0300

    adding image to the panel

--- code/Game.py
+++ code/Game.py
@@ -8,9 +8,9 @@ from code.menu import Menu
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode((600, 480))
+        self.window = pygame.display.set_mode(size=(600, 480))
 
-    def run(self, ):
+    def run(self):
         while True:
             menu = Menu(self.window)
             menu.run()
